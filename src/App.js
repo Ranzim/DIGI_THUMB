fec1e965217687595729693670d86a4e1988505a
Ranzim <ranzimthekare123@gmail.com>
2025-03-30 18:27:39 +0545

    old deleted file and due  to tailwind conflict

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind is Working!
+      </h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
